b451248eba30842542370b9d3fdcec0a29fa2ef7
willianchu <willianchu@hotmail.com>
2022-01-12 16:52:23 -0800

    update with lighthouse test

--- countLetters.js
+++ countLetters.js
@@ -26,3 +26,18 @@ const basic = "aeiouaeiouBC";
 const answer = countLetters(basic);
 assertEqual(answer["a"],2);
 assertEqual(answer["B"],1);
+
+console.log(countLetters("lighthouse in the house"));
+
+/* {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}*/
