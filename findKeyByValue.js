36d5fec9d06543749fe5b3fa0031505ea44671c8
willianchu <willianchu@hotmail.com>
2022-01-12 17:45:58 -0800

    add an object key finder by value returning the first key

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// Auxiliary code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Main Code
+const findKeyByValue = (targetObject, valueSearched) => {
+  for (const genre in targetObject) {
+    if (targetObject[genre] === valueSearched) {
+      return genre;
+    }
+  }
+};
+
+
+// Test code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
