a3da0a0078213cea03d0024d69c7fc9cec1fbdd9
Willian Chu <willianchu@live.com>
2022-01-13 18:26:15 -0800

    vs code pending update

--- eqArrays.js
+++ eqArrays.js
@@ -22,5 +22,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should NOT PASS
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // =>should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should NOT PASS
+assertEqual(eqArrays([3, "1", "2"], ["1", "2", 3]), true); // => should NOT PASS
+
+
 
